b2c3d28f9527187971e4ce67510efa1115d92410
Jon Brighton <brightonsbox@hotmail.com>
2020-08-09 14:26:01 +0100

    Temp removal of cloudfront

--- main.tf
+++ main.tf
@@ -61,60 +61,6 @@ resource "aws_acm_certificate" "cert" {
   subject_alternative_names = [var.bucket]
 }
 
-resource "aws_cloudfront_distribution" "s3_distribution" {
-
-  provider = "aws.us-east-1"
-  origin {
-    custom_origin_config {
-      http_port              = "80"
-      https_port             = "443"
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
-
-    domain_name = "${aws_s3_bucket.htts.website_endpoint}"
-    origin_id   = var.domain
-  }
-
-  enabled             = true
-  default_root_object = "index.html"
-
-  default_cache_behavior {
-    viewer_protocol_policy = "redirect-to-https"
-    compress               = true
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = var.domain
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  aliases = [var.domain]
-
-  viewer_certificate {
-    acm_certificate_arn      = "${aws_acm_certificate.cert.arn}"
-    minimum_protocol_version = "TLSv1"
-    ssl_support_method       = "sni-only"
-  }
-  depends_on = [
-    aws_acm_certificate.cert
-  ]
-}
-
 output "bucket-endpoint" {
   value = aws_s3_bucket.htts.website_endpoint
 }
